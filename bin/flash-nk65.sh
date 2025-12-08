4034ee6fb8b88557dad77d67e71e3ec10c8eebdb
Selene ToyKeeper <git@toykeeper.net>
2025-12-08 06:08:11 -0700

    imported Novelkeys NK65 files from my old repo
    Currently broken because I haven't ported it to QMK v0.31 yet.
    It was last updated for QMK v0.19, at which time this board still used
    Wilbatech RGB instead of the main RGB matrix code.  But QMK fixed that,
    and I haven't updated my code yet.

---
+++ bin/flash-nk65.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+MAKER=novelkeys
+MODEL=nk65
+MAP=toykeeper
+BRANCH=toykeeper-keymaps
+BACKUP=$HOME/src/qmk/builds
+set -e
+echo "Put the keyboard into reflashing state now..."
+sleep 3
+
+cd ~/src/qmk/qmk_firmware/
+source .venv/bin/activate
+#cd ~/src/qmk/keychron/
+#git co $BRANCH
+#make $MAKER/$MODEL:$MAP:flash
+qmk flash -kb $MAKER/$MODEL -km $MAP
+
+BUILD=$(basename $(/bin/ls -t  *.bin | head -1) .bin)
+if [ -n "$BUILD" ]; then
+  NOW=$(date +"%Y-%m-%d_%H%M")
+  cp -v "$BUILD".bin "$BACKUP"/"$BUILD"."$NOW".bin
+fi
+echo -n 'Press Enter on the flashed keyboard:' ; read FOO
+modmap
+#sleep 1
+#qwerty-dvorak.sh dvorak
---
+++ keyboards/novelkeys/nk65/keymaps/toykeeper/rules.mk
@@ -0,0 +1,5 @@
+# Compile options for ToyKeeper's NK65 layout
+# Copyright 2022-2025 Selene ToyKeeper
+# SPDX-License-Identifier: GPL-3.0-or-later
+# (empty because everything is in users/toykeeper/rules.mk)
+
